0449d2127c63bb7dd5f6bcda633f13bbda50e43e
flytecc <dlxorud12567@gmail.com>
2025-10-30 14:48:31 +0900

    Update app1.py

--- app1.py
+++
@@ -1,9 +0,0 @@
-for n in range(1, 101):
-    if n % 2 == 0 and n % 5 == 0:
-        print("2와 5의 배수입니다")
-    elif n % 2 == 0:
-        print("2의 배수입니다")
-    elif n % 5 == 0:
-        print("5의 배수입니다")
-    else:
-        print(n)
